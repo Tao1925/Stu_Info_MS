a26a820b0f60c0d60eb9d64fce3db96fa35bdac3
Tao1925 <yhxxtsy@163.com>
2021-05-28 11:26:38 +0800

    finish Bili SIMS episode 4

--- src/LoginStart.java
+++ src/LoginStart.java
@@ -3,16 +3,20 @@ import java.awt.*;
 
 public class LoginStart extends JFrame {
 
+    JLabel bg_img_label;
+    JPanel bg_img_panel;
+
     final int WIDTH = 600;
     final int HEIGHT = 400;
 
     public LoginStart(){
+        // init() func should be at first
+        init();
         setVisible(true);
-
         setResizable(false);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         validate(); // make components work
-        init();
+        //init();
     }
 
     void init(){
@@ -25,6 +29,17 @@ public class LoginStart extends JFrame {
         // make window displays at center
         int x = (width - WIDTH) / 2;
         int y = (height - HEIGHT) / 2;
-        setBounds(x, y, WIDTH, HEIGHT);
+        this.setBounds(x, y, WIDTH, HEIGHT);
+
+        // set background picture
+        bg_img_panel = new JPanel();
+        bg_img_panel.setBounds(0, 0, WIDTH, HEIGHT);
+        bg_img_panel.setLayout(null);
+        ImageIcon bg_img = new ImageIcon("src/img/bg_img.jpg");
+        bg_img_label = new JLabel(bg_img);
+        bg_img_label.setBounds(0,0,WIDTH,HEIGHT);
+        bg_img_panel.add(bg_img_label);
+        this.add(bg_img_panel);
+
     }
 }
